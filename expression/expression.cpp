21cba6b9fee84164e67c9522fa2bee9d5923b54f
personal <crooked@localhost.localdomain>
2014-04-24 23:35:26 +0800

    add some note

--- expression/expression.cpp
+++ expression/expression.cpp
@@ -8,7 +8,13 @@
 using namespace std;
 
 string getPostfixExp(string &infix)  //获取后缀表达式
-{
+{/*
+  转换原则：
+  1.当读到一个操作数时，立即将它放到输出中。操作符则不立即输出，放入栈中，遇到左圆括号也推入栈中。
+  2.如果遇到一个右括号，那么就将栈顶元素弹出，将符号写出直到遇到一个对应的左括号，但是这个左括号只被弹出，并不输出。
+  3.在读到操作符时，如果此时栈顶操作符优先级大于或等于此操作符，弹出栈顶操作符直到发现优先级更低的元素位置，除了处理“）”的时候，否则绝不从栈中移走“（”。操作符中，+-优先级最低，（）优先级最高。
+  4.如果读到输入的末尾，将栈元素弹出直到该栈变成空栈，将符号写到输出中。
+	*/
   stack<char> operator_stack;
   string postfix;
   for(int i=0;i<infix.size();i++)
@@ -104,7 +110,7 @@ int postfixCalculate(string &postfix)   //后缀表达所求值
 		 case ' ':
 			 break;
 		 default:
-			 num_stack.push(postfix[i]-'0');
+			 num_stack.push(postfix[i]-'0');   //读入操作数
              break;
 	 }
    }
